f48588b1d7cb56e505a12f740c2ecf17f035ae58
G <mirgita.frasheri@eng.au.dk>
2021-02-04 17:31:38 +0100

    full watertank simulation with fault inject

---
+++ faultinject-bundle/src/test/java/org/intocps/maestro/faultinject/WaterTankTest.java
@@ -0,0 +1,40 @@
+package org.intocps.maestro.faultinject;
+
+
+import org.apache.commons.io.IOUtils;
+import org.intocps.maestro.typechecker.TypeChecker;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+
+public class WaterTankTest {
+    @Test
+    public void test() throws Exception {
+
+        final File faultInjectSpec = Paths.get("target", "watertanktest", "FaultInject.mabl").toFile();
+        faultInjectSpec.getParentFile().mkdirs();
+        try (final FileWriter writer = new FileWriter(faultInjectSpec)) {
+            IOUtils.copy(FaultInjectRuntimeModule.class.getResourceAsStream("FaultInject.mabl"), writer, StandardCharsets.UTF_8);
+        }
+
+        final File spec = Paths.get("target", "watertanktest", "watertank-casestudy.mabl").toFile();
+        try (final FileWriter writer = new FileWriter(spec)) {
+            IOUtils.copy(this.getClass().getResourceAsStream("/watertank-casestudy.mabl"), writer, StandardCharsets.UTF_8);
+        }
+
+        //TODO: the FMI2 copying and addition to the parse path can be skipped once mable is updated - next release
+        final File fmi2 = Paths.get("target", "watertanktest", "FMI2.mabl").toFile();
+        try (final FileWriter writer = new FileWriter(fmi2)) {
+            IOUtils.copy(TypeChecker.class.getResourceAsStream("FMI2.mabl"), writer, StandardCharsets.UTF_8);
+        }
+
+        //we just want to call main but that doesnt work with surfire as main calls .exit which is not allowed
+        org.intocps.maestro.Main.argumentHandler(
+                new String[]{"--verbose", "--interpret", fmi2.getAbsolutePath(), faultInjectSpec.getAbsolutePath(), spec.getAbsolutePath()});
+
+    }
+}
+
---
+++ faultinject-bundle/src/test/resources/singlewatertank-20sim/sources/fmiGUID.h
@@ -0,0 +1,1 @@
+#define FMI_GUID "{cfc65592-9ece-4563-9705-1581b6e7071c}" 
