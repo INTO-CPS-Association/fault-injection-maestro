a001a6648f23873bab301c42f2acc53daf545d99
Casper Thule <c.thulehansen@live.com>
2021-08-06 11:29:52 +0200

    Added dedicated main

---
+++ faultinject-incubator/src/main/java/org/intocps/maestro/faultinject/incubator/Main.java
@@ -0,0 +1,38 @@
+package org.intocps.maestro.faultinject.incubator;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Main {
+
+    static final String faultInjectMablFileName = "FaultInject.mabl";
+    static final String faultInjectMablResourcePath = "org/intocps/maestro/faultinject/" + faultInjectMablFileName;
+    /**
+     * Accepts 2 arguments: multimodel and simulation json
+     * @param args
+     */
+    public static void main(String[] args) throws IOException {
+        if(args.length == 3) {
+            String initializePath = args[0];
+            String simulationJsonPath = args[1];
+            String dumpPath = args[2];
+
+            //Find the FaultInject.mabl
+            var faultInjectMabl = Main.class.getClassLoader().getResourceAsStream(faultInjectMablResourcePath);
+            // Copy it to the temporary folder on the laptop
+            var tempDir = Files.createTempDirectory("faultInjectIncubatorExample");
+            File faultInjectMablTargetFile = new File(tempDir.toFile(), faultInjectMablFileName);
+            FileUtils.copyInputStreamToFile(faultInjectMabl, faultInjectMablTargetFile);
+            System.out.println("Copied " + faultInjectMablFileName + " to: " + faultInjectMablTargetFile);
+            org.intocps.maestro.Main.argumentHandler(new String[]{"import","-output",dumpPath, "-i","Sg1",initializePath, simulationJsonPath,faultInjectMablTargetFile.getPath()} );
+
+        }
+        else
+        {
+            System.out.println("Expected 3 arguments, found " + args.length);
+        }
+    }
+}
--- faultinject-incubator/src/test/java/org/intocps/maestro/faultinject/rbmqfmuTest.java
+++ faultinject-incubator/src/test/java/org/intocps/maestro/faultinject/rbmqfmuTest.java
@@ -1,11 +1,13 @@
 package org.intocps.maestro.faultinject;
 
 import org.apache.commons.io.IOUtils;
+import org.intocps.maestro.faultinject.incubator.Main;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 
@@ -21,5 +23,14 @@ public class rbmqfmuTest {
             IOUtils.copy(FaultInjectRuntimeModule.class.getResourceAsStream("FaultInject.mabl"), writer, StandardCharsets.UTF_8);
         }
         org.intocps.maestro.Main.argumentHandler(new String[]{"import","-output",dumpPath, "-i","Sg1",initializePath, simulateJson,faultInjectSpec.getPath()} );
+    }
+
+    @Test
+    public void testMainWithConfig() throws IOException {
+        String initializePath = rbmqfmuTest.class.getClassLoader().getResource("multimodel.json").getPath();
+        String simulateJson = rbmqfmuTest.class.getClassLoader().getResource("coe.json").getPath();
+        String dumpPath = "target/test-classes/rbmq_example/dump";
+        Main.main(new String[]{initializePath, simulateJson, dumpPath});
+
     }
 }
