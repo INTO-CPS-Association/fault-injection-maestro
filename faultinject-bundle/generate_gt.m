c4d1c307fceb4f098a3338b0c9d679ecca3cffe6
gitting-around <gitting-around@users.noreply.github.com>
2021-09-13 17:06:22 +0200

    added one correctness test, with gt data, and matlab script for generating the gt data

---
+++ faultinject-bundle/generate_gt.m
@@ -0,0 +1,30 @@
+%% Generate ground truth data
+clear;
+time = 0.1:0.1:19.9;
+boolout = strings(1,199)';
+realout = zeros(1,199)';
+intout = zeros(1,199)';
+stringout = strings(1,199)';
+
+for i= 1:199
+   boolout(i) = 'false';
+   stringout(i) = 'bahamas';
+end
+
+% inject bool t=0.8, flip to true
+boolout(80) = 'true';
+
+% inject real 0.2<=t<0.5, by rule t+2*var_3
+for i=2:4
+    realout(i)= i*0.1 + 2*realout(i-1);
+end
+
+% inject int t>=10.0, by rule var_5+35
+for i=100:199
+    intout(i)= intout(i-1) + 35;
+end
+
+% inject string t=12.0 to halløj
+stringout(120) = 'halløj';
+
+writematrix([time', boolout, realout, intout, stringout],'output_ground_truth.csv')
--- faultinject-bundle/src/test/java/org/intocps/maestro/faultinject/injectCorrectnessTest.java
+++ faultinject-bundle/src/test/java/org/intocps/maestro/faultinject/injectCorrectnessTest.java
@@ -10,6 +10,8 @@ import java.io.FileWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.*;  
 import java.util.Scanner;  
 
@@ -40,15 +42,31 @@ public class injectCorrectnessTest {
         org.intocps.maestro.Main.argumentHandler(
                 new String[]{"interpret", "--verbose", fmi2.getAbsolutePath(), faultInjectSpec.getAbsolutePath(), spec.getAbsolutePath()});
 
-        //parsing a CSV file into Scanner class constructor  
-        // time boolout realout intout stringout boolin realin intin stringin
-        Scanner sc = new Scanner(new File("outputs.csv"));  
-        sc.useDelimiter(",");   //sets the delimiter pattern  
+        //csv file containing data
+        BufferedReader br = new BufferedReader(new FileReader("outputs.csv"));
+        String line;
 
-        while (sc.hasNext())  //returns a boolean value  
-        {  
-            System.out.print(sc.next());  //find and returns the next complete token from this scanner  
-        }   
-        sc.close();  //closes the scanner  
+        BufferedReader br2 = new BufferedReader(new FileReader("output_ground_truth.csv"));
+        String line2;
+        line = br.readLine(); // get rid of first header line
+        while ((line = br.readLine()) != null) {
+            // use comma as separator
+            String[] cols = line.split(",");
+            if((line2 = br2.readLine()) != null){
+
+                String[] cols2 = line2.split(",");
+
+                assertEquals(cols2[1], cols[1]);
+                assertEquals(cols2[3], cols[3]);
+                assertEquals(cols2[4], cols[4]);
+                //before comparing the reals, turn 0 to 0.0
+                if(cols2[2].compareTo("0")==0){
+                    cols2[2] = "0.0";
+                }
+                Double val = Double.parseDouble(cols[2]);
+                val = Math.round(val * 10.0) / 10.0;
+                assertEquals(cols2[2], Double.toString(val));
+            }
+        }
     }
 }
